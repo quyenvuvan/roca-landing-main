0861879f481b20a63b18c367522c0f4056115fcd
quyen <vuvanquyen3003@gmail.com>
2025-10-21 14:23:47 +0700

    seven commit

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -64,17 +64,19 @@ export default function Footer() {
             {/* Embedded Map */}
             <div className="flex flex-col items-center justify-center">
               <h5 className="font-semibold text-white mb-2">Bản đồ</h5>
-              <iframe
-                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3724.1234!2d106.6833!3d20.8449!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x314a7b8b8b8b8b8b%3A0x1234567890abcdef!2sVimos+Mart+c%E1%BB%ADa+h%C3%A0ng+ti%E1%BB%87n+%C3%ADch+Dr+Giang!5e0!3m2!1sen!2s!4v1234567890!5m2!1sen!2s"
-                width="300"
-                height="200"
-                style={{ border: 0 }}
-                allowFullScreen
-                loading="lazy"
-                referrerPolicy="no-referrer-when-downgrade"
-                className="rounded-lg"
-                title="Location Map"
-              ></iframe>
+              <div className="w-full max-w-xs md:max-w-sm">
+                <iframe
+                  src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3724.1234!2d106.6833!3d20.8449!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x314a7b8b8b8b8b8b%3A0x1234567890abcdef!2sVimos+Mart+c%E1%BB%ADa+h%C3%A0ng+ti%E1%BB%87n+%C3%ADch+Dr+Giang!5e0!3m2!1sen!2s!4v1234567890!5m2!1sen!2s"
+                  width="100%"
+                  height="200"
+                  style={{ border: 0 }}
+                  allowFullScreen
+                  loading="lazy"
+                  referrerPolicy="no-referrer-when-downgrade"
+                  className="rounded-lg"
+                  title="Location Map"
+                ></iframe>
+              </div>
               <p className="text-xs text-gray-300 mt-2 text-center">Nhấn vào bản đồ để xem chi tiết</p>
             </div>
           </div>
